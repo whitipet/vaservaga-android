72f1de98f6177079138c0ff65339a4cdd56aa73b
whitipet <whitipet@gmail.com>
2020-11-21 20:37:32 +0200

    Add simple LevelView

---
+++ app/src/main/java/com/whitipet/vaservaga/LevelView.java
@@ -0,0 +1,24 @@
+package com.whitipet.vaservaga;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+class LevelView extends View {
+
+	public LevelView(Context context) {
+		super(context);
+	}
+
+	public LevelView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public LevelView(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+	}
+
+	public LevelView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+		super(context, attrs, defStyleAttr, defStyleRes);
+	}
+}
--- app/src/main/java/com/whitipet/vaservaga/MainActivity.java
+++ app/src/main/java/com/whitipet/vaservaga/MainActivity.java
@@ -2,12 +2,17 @@ package com.whitipet.vaservaga;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(new LevelView(this));
+
+		View decorView = getWindow().getDecorView();
+		decorView.setSystemUiVisibility(decorView.getSystemUiVisibility()
+				| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+	}
 }
